3550e0b6a4d35ca18afcdabdcca765720d31e38d
VictorBiga <victor.biga@gmail.com>
2018-10-01 17:24:28 +0100

    added header

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#Script Name	:install_ansible.sh
+#Description	:This script will install ansible on EC2 instance
+#Run where      :AWS
+#Author       	:Victor Biga                                                
+#Email         	:victor.biga@gmail.com                                           
+#Version info   :1.0
+#Test date      :
+#Test result    :NA
+
 pip install --upgrade pip
 pip install ansible
