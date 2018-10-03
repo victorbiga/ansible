018ed475ef3c3faf51cd7ee2d9329ca4f6cbedf5
VictorBiga <victor.biga@gmail.com>
2018-10-03 10:32:48 +0100

    test ok

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#Script Name	:install_ansible.sh
-#Description	:This script will install ansible on EC2 instance
-#Run where      :AWS EC2 AMI
-#Author       	:Victor Biga                                                
-#Email         	:victor.biga@gmail.com                                           
-#Version info   :1.0
-#Test date      :
-#Test result    :NA
+#Script Name:	install_ansible.sh
+#Description:	This script will install ansible on EC2 instance
+#Run where:	AWS EC2 AMI
+#Author:	Victor Biga                                                
+#Email:		victor.biga@gmail.com                                           
+#Version info:	1.0
+#Test date:	02/10/2018
+#Test result:	OK
 
 
 #Become root, update AMI, enable epel release including updating repo app list and install ansible
