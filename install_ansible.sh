2c6758bc28bad5da0b2499d5a24cdf5df4d1af14
VictorBiga <victor.biga@gmail.com>
2018-10-03 10:30:08 +0100

    last tweaks and test complete on aws ami

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,14 +2,17 @@
 
 #Script Name	:install_ansible.sh
 #Description	:This script will install ansible on EC2 instance
-#Run where      :AWS
+#Run where      :AWS EC2 AMI
 #Author       	:Victor Biga                                                
 #Email         	:victor.biga@gmail.com                                           
 #Version info   :1.0
 #Test date      :
 #Test result    :NA
 
+
+#Become root, update AMI, enable epel release including updating repo app list and install ansible
 sudo su
 yum update -y
-pip install --upgrade pip
-pip install ansible
+yum-config-manager --enable epel
+yum repolist
+yum install ansible -y
