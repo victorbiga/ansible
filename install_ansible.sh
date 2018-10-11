69a3d10377e079d8d97eb4ee9c5a197755d60709
VictorBiga <victor.biga@gmail.com>
2018-10-11 11:16:37 +0100

    added automated jenkins installation and nagios4 docker images

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #Script Name:	install_ansible.sh
-#Description:	This script will install ansible on EC2 instance
+#Description:	This script will install ansible, docker, git and start 2 containers - nagios4 and automated jenkins installation with default users  on EC2 instance
 #Run where:	AWS EC2 AMI
 #Author:	Victor Biga                                                
 #Email:		victor.biga@gmail.com                                           
 #Version info:	1.0
 #Test date:	02/10/2018
 #Test result:	OK
+#nagios username: nagiosadmin password: nagios
+#jenkins username: admin password: admin
 
 
 #Become root, update AMI, enable epel release including updating repo app list and install ansible
@@ -18,7 +20,18 @@ yum repolist
 yum install ansible -y
 yum install git -y
 yum install docker -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-
+service docker start
+usermod -a -G docker ec2-user
+reboot
+docker run \
+  --name auto-jenkins \
+  --publish 0.0.0.0:8080:8080 \
+  --detach \
+  --restart always \
+  depositsolutions/jenkins-automation
+sleep 10
+docker run \ 
+  --name nagios4 \
+  --publish 0.0.0.0:80:80 \ 
+  --restart always \ 
+  jasonrivers/nagios:latest
